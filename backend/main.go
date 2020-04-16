95fce06f30fdc41130e00645ca3eba6664b07b9f
Evan Chan <evan.chanyiksan@gmail.com>
2020-04-15 20:48:16 -0700

    http GET and POST

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,76 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
 )
 
+type Handler struct {
+	balance int
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Received request %s %s", r.Method, r.URL)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (h *Handler) GetBalance(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	if r.Method == http.MethodOptions {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	body := fmt.Sprintf("{\"balance\": %d}", h.balance)
+	w.WriteHeader(http.StatusOK)
+	if _, err := io.WriteString(w, body); err != nil {
+		log.Printf("failed to write balance result, error=%s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *Handler) Deposit(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	if r.Method == http.MethodOptions {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	deposit, err := strconv.Atoi(vars["deposit"])
+	if err != nil {
+		log.Printf("failed to parse deposit %s", vars["deposit"])
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	h.balance += deposit
+	w.WriteHeader(http.StatusOK)
+	return
+}
+
 func main() {
-	fmt.Println("hello world!")
+	router := mux.NewRouter()
+	handler := Handler{
+		balance: 42,
+	}
+	router.HandleFunc("/balance", handler.GetBalance)
+	router.HandleFunc("/deposit/{deposit}", handler.Deposit).Methods(http.MethodPost)
+	router.Use(mux.CORSMethodMiddleware(router))
+	router.Use(loggingMiddleware)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:8080",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-const App = () => (
-    <div>Hello world!</div>
-)
+const App = () => <BankAccount />;
+
+const BankAccount = () => {
+  const [balance, setBalance] = useState<number | null>(null);
+  const [deposit, setDeposit] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8080/balance");
+      const data = await response.json(); // {"balance": 42}
+      setBalance(data.balance);
+    };
+    fetchData();
+  }, []);
+
+  // @ts-ignore
+  const handleDeposit = async (event) => {
+    const response = await fetch("http://localhost:8080/deposit/" + deposit, {
+      method: "POST",
+    });
+    await response.json();
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <div>Your balance is {balance || "unknown"}</div>
+      <form onSubmit={handleDeposit}>
+        <label>
+          Deposit:
+          <input
+            type="text"
+            value={deposit}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setDeposit(+event.target.value)
+            }
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
 
 export default App;
